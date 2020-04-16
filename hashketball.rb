d34e3fbb8b677f29476c83feea9969e6a9832f88
edwin calvillo <calvillo.edwin@gmail.com>
2020-04-16 04:09:11 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,9 +1,58 @@
 def game_hash
     hash = {
       home: = {
-        team_name: "",
-        colors: [],
+        team_name: "Booklyn Nets",
+        colors: ["Black", "White"],
         players: [
+          # Player 1
+          {
+            player_name: "Alan Anderson",
+            number: "0",
+            shoe: "16",
+            points: "22",
+            rebounds: "12",
+            assists: "12",
+            steals: "3",
+            blocks: "1",
+            slam_dunks: "1"
+          },
+          # Player 2
+          {
+            player_name: "Reggie Evans",
+            number: "30",
+            shoe: "14",
+            points: "12",
+            rebounds: "12",
+            assists: "12",
+            steals: "12",
+            blocks: "12",
+            slam_dunks: "7"
+          },
+          # Player 3
+          {
+            player_name: "",
+            number: "",
+            shoe: "",
+            points: "",
+            rebounds: "",
+            assists: "",
+            steals: "",
+            blocks: "",
+            slam_dunks: ""
+          },
+          # Player 4
+          {
+            player_name: "",
+            number: "",
+            shoe: "",
+            points: "",
+            rebounds: "",
+            assists: "",
+            steals: "",
+            blocks: "",
+            slam_dunks: ""
+          },
+          # Player 5
           {
             player_name: "",
             number: "",
@@ -19,9 +68,70 @@ def game_hash
       }
 
       away: = {
-        team_name: "",
-        colors: [],
-        players: []
+        team_name: "Charlotte Hornets",
+        colors: ["Turquoise", "Purple"],
+        players: [
+          # Player 1
+          {
+            player_name: "",
+            number: "",
+            shoe: "",
+            points: "",
+            rebounds: "",
+            assists: "",
+            steals: "",
+            blocks: "",
+            slam_dunks: ""
+          },
+          # Player 2
+          {
+            player_name: "",
+            number: "",
+            shoe: "",
+            points: "",
+            rebounds: "",
+            assists: "",
+            steals: "",
+            blocks: "",
+            slam_dunks: ""
+          },
+          # Player 3
+          {
+            player_name: "",
+            number: "",
+            shoe: "",
+            points: "",
+            rebounds: "",
+            assists: "",
+            steals: "",
+            blocks: "",
+            slam_dunks: ""
+          },
+          # Player 4
+          {
+            player_name: "",
+            number: "",
+            shoe: "",
+            points: "",
+            rebounds: "",
+            assists: "",
+            steals: "",
+            blocks: "",
+            slam_dunks: ""
+          },
+          # Player 5
+          {
+            player_name: "",
+            number: "",
+            shoe: "",
+            points: "",
+            rebounds: "",
+            assists: "",
+            steals: "",
+            blocks: "",
+            slam_dunks: ""
+          }
+        ]
       }
     }
 end
