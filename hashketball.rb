56d4fa1fd4b36a24fe8f70988a8593f8dd99853d
edwin calvillo <calvillo.edwin@gmail.com>
2020-04-16 04:14:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -73,63 +73,63 @@ def game_hash
         players: [
           # Player 1
           {
-            player_name: "",
-            number: "",
-            shoe: "",
-            points: "",
-            rebounds: "",
-            assists: "",
-            steals: "",
-            blocks: "",
-            slam_dunks: ""
+            player_name: "Jeff Adrien",
+            number: "4",
+            shoe: "18",
+            points: "10",
+            rebounds: "1",
+            assists: "1",
+            steals: "2",
+            blocks: "7",
+            slam_dunks: "2"
           },
           # Player 2
           {
-            player_name: "",
-            number: "",
-            shoe: "",
-            points: "",
-            rebounds: "",
-            assists: "",
-            steals: "",
-            blocks: "",
-            slam_dunks: ""
+            player_name: "Bismack Biyombo",
+            number: "0",
+            shoe: "16",
+            points: "12",
+            rebounds: "4",
+            assists: "7",
+            steals: "22",
+            blocks: "15",
+            slam_dunks: "10"
           },
           # Player 3
           {
-            player_name: "",
-            number: "",
-            shoe: "",
-            points: "",
-            rebounds: "",
-            assists: "",
-            steals: "",
-            blocks: "",
-            slam_dunks: ""
+            player_name: "DeSagna Diop",
+            number: "2",
+            shoe: "14",
+            points: "24",
+            rebounds: "12",
+            assists: "12",
+            steals: "4",
+            blocks: "5",
+            slam_dunks: "5"
           },
           # Player 4
           {
-            player_name: "",
-            number: "",
-            shoe: "",
-            points: "",
-            rebounds: "",
-            assists: "",
-            steals: "",
-            blocks: "",
-            slam_dunks: ""
+            player_name: "Ben Gordon",
+            number: "8",
+            shoe: "15",
+            points: "33",
+            rebounds: "3",
+            assists: "2",
+            steals: "1",
+            blocks: "1",
+            slam_dunks: "0"
           },
           # Player 5
           {
-            player_name: "",
-            number: "",
-            shoe: "",
-            points: "",
-            rebounds: "",
-            assists: "",
-            steals: "",
-            blocks: "",
-            slam_dunks: ""
+            player_name: "Kemba Walker",
+            number: "33",
+            shoe: "15",
+            points: "6",
+            rebounds: "12",
+            assists: "12",
+            steals: "7",
+            blocks: "5",
+            slam_dunks: "12"
           }
         ]
       }
